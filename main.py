e84f9c9a97b8fb0b30b752aa1fa60893c7662ca4
najla-alosime <najlaalosime@gmail.com>
2024-05-29 14:22:57 +0300

    najla

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+import joblib
+model = joblib.load('Models/knn_model.joblib')
+scaler = joblib.load('Models/scaler.joblib')
+
+
+from pydantic import BaseModel
+# Define a Pydantic model for input data validation
+class InputFeatures(BaseModel):
+    Year: int
+    Engine_Size: float
+    Mileage: float
+    Type: str
+    Make: str
+    Options: str
+
+
+
+
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+    'Year': input_features.Year,
+    'Engine_Size': input_features.Engine_Size,
+    'Mileage': input_features.Mileage,
+    'Type_Accent': input_features.Type == 'Accent',
+    'Type_Land Cruiser': input_features.Type == 'LandCruiser',
+    'Make_Hyundai': input_features.Make == 'Hyundai',
+    'Make_Mercedes': input_features.Make == 'Mercedes',
+    'Options_Full': input_features.Options == 'Full',
+    'Options_Standard': input_features.Options == 'Standard'
+    }
+    # Convert dictionary values to a list in the correct order
+    features_list = [dict_f[key] for key in sorted(dict_f)]
+    # Scale the input features
+    scaled_features = scaler.transform([list(dict_f.values
+    ())])
+    return scaled_features
+
+@app.post("/predict")
+async def predict(input_features: InputFeatures):
+    data = preprocessing(input_features)
+    y_pred = model.predict(data)
+    return {"pred": y_pred.tolist()[0]}
